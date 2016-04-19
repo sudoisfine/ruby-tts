72f78b6247a8bb4e4abda39d34b85e3436393ada
MELISSA GUION <M.GUION@GMAIL.COM>
2016-04-19 09:04:37 -0500

    adds logic for determining a winner and adds play method to rps.

--- rps.rb
+++ rps.rb
@@ -8,6 +8,13 @@ class RockPaperScissors
     ['scissors', 'paper']
   ]
 
+def play
+  human_move = human_selection
+  computer_move = computer_selection
+  get_winner(human_move, computer_move)
+end
+
+
   def human_selection
     puts "Rock, paper, or scissors?"
     gets.chomp.downcase
@@ -16,5 +23,18 @@ class RockPaperScissors
   def computer_selection
     PLAYS.sample
   end
+
+  def get_winner(input1, input2)
+    if input1 == input2
+      "It's a tie! Play again."
+    else
+      if WINS.include?([input1, input2])
+        "Computer shoots #{input2.capitalize}, and you shot #{input1.capitalize}. You win!"
+      else
+        "Computer shoots #{input2.capitalize}, and you shot #{input1.capitalize}. Computer wins."
+      end
+    end
+  end
+
 
   end
