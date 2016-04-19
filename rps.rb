0e39934e55884633f022421dadd38395ff2ef86b
MELISSA GUION <M.GUION@GMAIL.COM>
2016-04-19 08:52:02 -0500

    adds selection of humans and computers to rps

--- rps.rb
+++ rps.rb
@@ -1,1 +1,20 @@
 require "pry"
+
+class RockPaperScissors
+  PLAYS = ['rock', 'paper', 'scissors']
+  WINS = [
+    ['rock', 'scissors'],
+    ['paper', 'rock'],
+    ['scissors', 'paper']
+  ]
+
+  def human_selection
+    puts "Rock, paper, or scissors?"
+    gets.chomp.downcase
+  end
+
+  def computer_selection
+    PLAYS.sample
+  end
+
+  end
